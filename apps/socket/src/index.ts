f28dca9884f11cc09ff1a0108fc29dfe4522de17
Chirag Chandrashekhar <cchirag85@gmail.com>
2024-05-07 14:19:36 +0530

    feat: added basic events to the socket server and created a package that exports the socket client

--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -4,9 +4,44 @@ const SOCKET_PORT = 4000;
 const io = new Server(SOCKET_PORT);
 
 io.on('connection', (socket) => {
-  console.log('Connected');
-  socket.on('message', (whatever) => {
-    console.log(whatever);
-    socket.send(whatever);
+  console.log('Connected to Socket ID: ', socket.id);
+  socket.on('create room', async (message) => {
+    const { roomID } = JSON.parse(message);
+    await socket.join(roomID);
+    if (io.sockets.adapter.rooms.has(roomID)) {
+      console.log('Socket: ', socket.id, 'joined room: ', roomID);
+      socket.send('success');
+    } else {
+      console.log('Failed to join room: ', roomID);
+      socket.send('failed');
+    }
+  });
+
+  socket.on('join room', async (message) => {
+    const { roomID } = JSON.parse(message);
+    if (io.sockets.adapter.rooms.has(roomID)) {
+      await socket.join(roomID);
+      if (socket.rooms.has(roomID)) {
+        console.log('Socket: ', socket.id, 'joined room: ', roomID);
+        socket.send('success');
+      } else {
+        console.log('Failed to join room: ', roomID);
+        socket.send('failed');
+        socket.disconnect();
+      }
+    } else {
+      console.log('Room ', roomID, 'does not exist');
+      socket.send('failed');
+      socket.disconnect();
+    }
+  });
+
+  socket.on('sign', (message) => {
+    const { signature, roomID } = JSON.parse(message);
+    if (socket.rooms.has(roomID)) {
+      socket.to(roomID).emit('signature', signature);
+    } else {
+      socket.send('failed');
+    }
   });
 });
